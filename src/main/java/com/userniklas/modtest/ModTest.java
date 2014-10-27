27e1e4afa9e86db30154f846e8094cd58e9f6895
Niklas <brunhildegard51@gmail.com>
2014-10-27 18:22:59 +0100

    Update

--- src/main/java/com/userniklas/modtest/ModTest.java
+++ src/main/java/com/userniklas/modtest/ModTest.java
@@ -1,9 +1,29 @@
 package com.userniklas.modtest;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "ModTest", name = "Mod Test", version = "1.7.10-1.0")
 public class ModTest 
 {
+	@Mod.EventHandler()
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
 	
+	@Mod.EventHandler()
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+	
+	@Mod.EventHandler()
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
 }
